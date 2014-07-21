cfabc87e102dca2ca4fd12facf40fe2b73f71f76
kimihito <tatsurotamashiro@gmail.com>
2014-07-21 11:51:34 +0900

    yo

--- app/controllers/yo_controller.rb
+++ app/controllers/yo_controller.rb
@@ -1,4 +1,20 @@
 class YoController < ApplicationController
   def index
+    yo_api = ENV['YO_API_KEY']
+    @username = params['username']
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    end
+    yo = Hey::Yo.new(api_token: yo_api)
+    begin
+      yo.user @username
+    rescue
+      client.update("#{@username} のYoがお盛んです")
+    else
+      client.update ("Yo via #{@username}")
+    end
   end
 end
